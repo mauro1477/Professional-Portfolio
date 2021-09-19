2c8af2c101dcf342193495abfccce50db3124445
Mauro Vargas <maurovargas7725@gmail.com>
2021-09-19 17:49:26 -0600

    add public directory and change profile to /public

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-apache2 header.php
+web: vendor/bin/heroku-php-apache2 public/
--- public/header.php
+++ public/header.php
@@ -1,4 +1,4 @@
-<?php require('vendor/autoload.php'); ?>
+<?php require('../vendor/autoload.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
     <html>
