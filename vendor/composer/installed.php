1b1c526b1fdf8dc59140d367d199246bd1827218
Mauro Vargas <maurovargas7725@gmail.com>
2021-09-19 17:15:56 -0600

    added composer.json file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7e08b88dbbbf426b3a0d53353cc674e90e09193e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7e08b88dbbbf426b3a0d53353cc674e90e09193e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
